9aa3769403148214b7a4418cc50418e2bf117540
van viet <viet.ngovanviet@gmail.com>
2021-07-26 23:00:01 +0700

    oke

--- lop.h
+++ lop.h
@@ -1,5 +1,16 @@
+#ifndef      __lop_H__
+#define      __lop_H__
 
 
+#include"dataType.h"
+#include"capLop.h"
+/*    
+Lớp: danh sách liên kết đơn (Mã lớp, trạng thái, Phòng học, contrỏ). Mỗi lớp
+có 1 mã duy nhất; trạng thái của lớp bao gồm: 0: lớp chuẩn bị mở, 1: lớp đang 
+học, 2: lớp đã hoàn tất. Thuộc tính contrỏ là con trỏ chỉ đến danh sách học 
+viên của lớp (con trỏ chỉ đến cây nhị phân tìm kiếm HọcViên).
+*/
+	  
 
 
 //main  function 
