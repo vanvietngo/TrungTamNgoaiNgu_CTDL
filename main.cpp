5dcdc663c8989f3dc6fcae062bffd23c930c0b1d
van viet <viet.ngovanviet@gmail.com>
2021-08-02 00:52:30 +0700

    fix readfile hoc vien

--- main.cpp
+++ main.cpp
@@ -33,39 +33,39 @@ int main() {
         strcat(fileLop, ".txt");
         openFileLop(ds_CapLop.dsCapLop[count0]->ds_Lop, fileLop);
 
-        // // open file Hoc Vien
-        // LOP* lop;
-		// lop = ds_CapLop.dsCapLop[count0]->ds_Lop.pHead_Lop;
-        // while(lop != NULL)  {
-        //     cout<<"InWhile : lop->Ma not null = "<<count0<<"  "<<lop->Ma<<endl;
-        //     // if(lop->ds_HocVien.root != NULL)    {
-        //         // tao file HV
-        //         strcpy(fileHocVien, xxx);
-        //         strcat(fileHocVien,urlDB);
-        //         strcat(fileHocVien, lop->Ma);
-        //         strcat(fileHocVien, ".txt");  
-        //         openFileHocVien(lop->ds_HocVien, fileHocVien);
-        //     // }
-        //     lop = lop->pNext;
-        // }
-    }
-    for(int countLOP =0; countLOP < ds_CapLop.count; countLOP++)	{
-                // // open file Hoc Vien
+        // open file Hoc Vien
         LOP* lop;
-		lop = ds_CapLop.dsCapLop[countLOP]->ds_Lop.pHead_Lop;
+		lop = ds_CapLop.dsCapLop[count0]->ds_Lop.pHead_Lop;
         while(lop != NULL)  {
-            cout<<"InWhile : lop->Ma not null = "<<countLOP<<"  "<<lop->Ma<<endl;
-            // if(lop->ds_HocVien.root != NULL)    {
+            cout<<"InWhile : lop->Ma not null = "<<count0<<"  "<<lop->Ma<<endl;
+            if(lop->ds_HocVien.root != NULL)    {
                 // tao file HV
                 strcpy(fileHocVien, xxx);
                 strcat(fileHocVien,urlDB);
                 strcat(fileHocVien, lop->Ma);
                 strcat(fileHocVien, ".txt");  
                 openFileHocVien(lop->ds_HocVien, fileHocVien);
-            // }
+            }
             lop = lop->pNext;
         }
     }
+    // for(int countLOP =0; countLOP < ds_CapLop.count; countLOP++)	{
+    //             // // open file Hoc Vien
+    //     LOP* lop;
+	// 	lop = ds_CapLop.dsCapLop[countLOP]->ds_Lop.pHead_Lop;
+    //     while(lop != NULL)  {
+    //         cout<<"InWhile : lop->Ma not null = "<<countLOP<<"  "<<lop->Ma<<endl;
+    //         // if(lop->ds_HocVien.root != NULL)    {
+    //             // tao file HV
+    //             strcpy(fileHocVien, xxx);
+    //             strcat(fileHocVien,urlDB);
+    //             strcat(fileHocVien, lop->Ma);
+    //             strcat(fileHocVien, ".txt");  
+    //             openFileHocVien(lop->ds_HocVien, fileHocVien);
+    //         // }
+    //         lop = lop->pNext;
+    //     }
+    // }
 
     while (kt_choice) {
         cout << endl << endl << endl;
